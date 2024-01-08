d8a9b4b1e343313f639a365222d9a55d227f653a
Vitali Plagov <vplagov@pm.me>
2024-01-08 22:44:14 +0200

    Initial commit

---
+++ src/main/java/io/plagov/loandecisionengine/Application.java
@@ -0,0 +1,13 @@
+package io.plagov.loandecisionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
